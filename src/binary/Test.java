2e258fbf90deaceea975734f99ef72f819ebf888
kimyeon96 <flflfkfk@naver.com>
2022-03-16 00:21:05 +0900

    명시적형변환

---
+++ src/binary/Test.java
@@ -0,0 +1,17 @@
+package binary;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// 지역변수, 전역변수, for문 문장 구조, for문으로 구구단
+	
+		// 구구단 -> 단(i) * 곱(j) = 결과
+		// 단 = 1 ~ 9 => for
+		// 곱 = 1 ~ 9 => for
+		// for (초기화값; 조건문; 증감식)
+
+
+		
+	}
+
+}
---
+++ src/binary/practice.java
@@ -0,0 +1,11 @@
+package binary;
+
+public class practice {
+
+	public static void main(String[] args) {
+		for(int i = 1; i<=9; i++) {
+		System.out.println(i);
+	}
+
+}
+}
